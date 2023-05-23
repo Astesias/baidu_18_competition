56a3ba709028eb90e2af7eb77a2e10e8d3071a4d
YSL <2264168148@qq.com>
2023-05-23 15:24:23 +0800

    main

--- Tasker.py
+++ Tasker.py
@@ -2,6 +2,7 @@ from multiprocessing import Process as pcs
 from multiprocessing import Queue 
 from pysl import Config,os_enter,easy_request
 import os,config_make,time
+from pprint import pprint
 
 from Baidu_18_ysl import run
 
@@ -9,6 +10,7 @@ Q_Order=Queue(maxsize=5)
 config_make.make_cfg()
 
 cfg=Config('./configs.json')
+pprint(cfg)
 server,port=cfg.server,cfg.port
 
 def server_tasker(server,port):
